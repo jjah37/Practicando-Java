2d8cc443a8a879191aef16d312cd4bb73882b91e
Jhon Jairo Arango Hurtado <39918619+jjah37@users.noreply.github.com>
2025-04-24 08:11:20 -0500

    Add files via upload
    El programa Calculadora.java está diseñado para realizar cálculos geométricos y matemáticos básicos. Incluye funcionalidades para trabajar con círculos, prismas y calcular la media de un conjunto de números. Las principales características del programa son:
    Círculo:
    Cálculo del área de un círculo dado su radio.
    Cálculo de la circunferencia de un círculo.
    Prisma:
    Cálculo del volumen de un prisma rectangular dadas sus dimensiones (largo, ancho y altura).
    Cálculo del área superficial de un prisma rectangular.
    Media (Promedio):
    Entrada de un conjunto de números por parte del usuario.
    Cálculo y visualización de la media aritmética de los números ingresados.

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+    double calcularAreaCirculo (double radio) {
+        return Math.PI * radio * radio;
+    }
+
+    double calcularAreaPrismaRectangular (double altura, double anchura, double profundidad) {
+        return altura * anchura * profundidad;
+    }
+
+    double calcularMedia (double ... numeros) {
+        if (numeros.length == 0) {
+            return 0.0;
+        }
+
+        double suma = 0.0;
+        for (int i = 0;i < numeros.length; i++) {
+            suma += numeros[i];
+        }
+        return suma / numeros.length;
+    }
+
+}
